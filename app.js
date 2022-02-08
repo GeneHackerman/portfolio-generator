d798941f4c74a2ad958d90d71a91046c752c6632
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-02-08 18:36:04 -0500

    created page-template

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+//     console.log('================');
 
-    console.log('================');
-
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+//     // is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
     
-};
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+// const generatePage = () => 'Name: Jane, Github: janehub'; this is the same as...
+
+// ...this
+
+// const generatePage = (userName, gitHubName) => `Name: ${userName}, GitHub: ${gitHubName}`;
+
+// console.log(generatePage('Jane', 'janehub'));
+
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+// three arguments: create html, data being written, error catcher
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+}); 
