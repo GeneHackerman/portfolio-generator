0f49fb83ab003f5a368a0586173eba284176e36f
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-02-09 20:15:40 -0500

    adding sections in html

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ const inquirer = require("inquirer");
 
 // console.log(generatePage('Jane', 'janehub'));
 
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+
 
 const promptUser = () => {
     
@@ -169,18 +173,15 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
+
+        // // three arguments: create html, data being written, error catcher
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+        }); 
+
+
+
         console.log(portfolioData);
-    })
-
-    
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// // three arguments: create html, data being written, error catcher
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw new Error(err);
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// }); 
+    });
